1d992298b6b9604ab857f88d12e3d0055fd2804d
manato-takahashi <manato.takahashi86@gmail.com>
2024-06-29 14:47:01 +0900

    add: migrationとmodelのファイルを作成

--- db/migrate/20240626131256_create_orders.rb
+++ db/migrate/20240626131256_create_orders.rb
@@ -1,7 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[7.0]
   def change
-    create_table :orders do |t|
-      t.string :name
+    create_table :orders, comment: '注文情報' do |t|
+      t.string :name, null: false, comment: 'お名前'
 
       t.timestamps
     end
